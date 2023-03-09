78ca6d8e8be43daa73206560c7dc3b18319af1ec
Codeverse <125686715+Codeverse2k23@users.noreply.github.com>
2023-03-09 15:15:46 +0530

    Add files via upload

---
+++ tabs.js
@@ -0,0 +1,9 @@
+var tabs = $('.tabs');
+var selector = $('.tabs').find('a').length;
+//var selector = $(".tabs").find(".selector");
+var activeItem = tabs.find('.active');
+var activeWidth = activeItem.innerWidth();
+$(".selector").css({
+  "left": activeItem.position.left + "px", 
+  "width": activeWidth + "px"
+});
