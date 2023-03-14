a2da058fb8bba623677f75cdce1599a7c7850c20
Codeverse <125686715+Codeverse2k23@users.noreply.github.com>
2023-03-14 14:27:06 +0530

    Delete tabs.js

--- tabs.js
+++
@@ -1,9 +0,0 @@
-var tabs = $('.tabs');
-var selector = $('.tabs').find('a').length;
-//var selector = $(".tabs").find(".selector");
-var activeItem = tabs.find('.active');
-var activeWidth = activeItem.innerWidth();
-$(".selector").css({
-  "left": activeItem.position.left + "px", 
-  "width": activeWidth + "px"
-});
